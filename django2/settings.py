b84cabef231249b3803444d3fbd0ee6da34f45e9
Paulo Henrique <ph.tavaresleite@gmail.com>
2024-12-05 17:44:22 -0300

    development true

--- django2/settings.py
+++ django2/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-29xzz$(n9+96^g)#_h(r@+3rv=9)2xi#p_!4b+&a#vg(j73pw9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
